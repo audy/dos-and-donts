34be8ccd3ea5bec7de3c1a8b1891e4b71d3ac207
Austin Davis-Richardson <harekrishna@gmail.com>
2015-08-26 23:20:35 -0400

    save images and text

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from lxml import html
+import shutil
 import requests
 
 def get_url(path, base='http://www.vice.com'):
@@ -22,19 +23,48 @@ def get_page(url):
 def get_body(page):
     return html.fromstring(page.text)
 
+def get_id(path):
+    return path.split('/')[-1]
+
+def download_image(url, destination):
+    response = requests.get(url, stream=True)
+    with open(destination, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    return destination
+
 def do_a_crawling(path):
     page = get_page(path)
     body = get_body(page)
 
     return {
+            'id': get_id(path),
             'image': get_image(body),
             'description': get_description(body),
-            'next_path': get_next_path(body)
+            'next_path': get_next_path(body),
             }
+
+def save_results(crawl):
+
+    # download image
+    image_url = crawl['image']
+    response = requests.get(image_url, stream=True)
+    image_destination = '%s.jpg' % crawl['id']
+    with open(image_destination, 'wb') as handle:
+        shutil.copyfileobj(response.raw, handle)
+
+    text_destination = '%s.txt'
+
+    # save text
+    with open(text_destination, 'w') as handle:
+        print >> handle, crawl['description']
 
 
 path = '/dnd'
 while True:
     resp = do_a_crawling(path)
     path = resp['next_path']
-    print do_a_crawling(path)
+    crawl = do_a_crawling(path)
+
+    print crawl
+
+    save_results(crawl)
